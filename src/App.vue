c76601743cbcd9adaddbed8544da1c7fe3d772d4
guddndhk <guddndhk@naver.com>
2022-01-28 23:38:47 +0900

    Vue.js study 스타일 클래스 등등 바인딩 작업

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,37 @@
         </div>
       </div>
 
-      <div v-show="hasError" style="color: red">This field cannot be empty</div>
+      <div v-show="hasError" style="color: red">
+        This field cannot be empty
+        </div>
     </form>
-    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2">
-        {{ todo.subject }}
+    <div v-if="!todos.length">
+      추가된 todo가 없습니다.
+    </div>
+    <div 
+      v-for="(todo, index) in todos" 
+      :key="todo.id" 
+      class="card mt-2"
+      >
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="todo.completed"
+          />
+          <label class="form-check-label" :class="{ todo: todo.completed }">
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+          <button 
+            class="btn btn-danger"
+            @click="deleteTodo(index)"
+            >
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -32,12 +58,12 @@ import { ref } from "vue";
 export default {
   setup() {
     const todo = ref("");
-    const todos = ref([
-      { di: 1, subject: "휴대폰사기" },
-      { di: 2, subject: "장보기" },
-    ]);
-
+    const todos = ref([]);
     const hasError = ref(false);
+    const todoStyle = {
+      textDecoration: "line-through",
+      color: "gray",
+    };
 
     const onSubmit = () => {
       if (todo.value == "") {
@@ -46,9 +72,15 @@ export default {
         todos.value.push({
           id: Date.now(),
           subject: todo.value,
+          completed: false,
         });
         hasError.value = false;
+        todo.value = "";
       }
+    };
+
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
     };
 
     return {
@@ -56,13 +88,16 @@ export default {
       onSubmit,
       todos,
       hasError,
+      todoStyle,
+      deleteTodo,
     };
   },
 };
 </script>
 
 <style>
-.name {
-  color: red;
+.todo {
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
