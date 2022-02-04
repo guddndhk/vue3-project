309b7c008e536c779b9badc4f7c7f1ec5d074bd1
guddndhk <guddndhk@naver.com>
2022-02-04 17:56:49 +0900

    22/02/04 computed 과 filter 함수를 이용해 search 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+     <input
+        class="form-control"
+        type="text"
+        v-model="searchText"
+        placeholder="Search"
+     >
+     <hr />
     <todo-simple-form @add-todo="addTodo" />
 
-    <div v-if="!todos.length">
-      추가된 todo가 없습니다.
+    <div v-if="!filteredTodos.length">
+      There is nothing to display
     </div>
     <todo-list 
-    :todos="todos" 
+    :todos="filteredTodos" 
     @toggle-todo="toggleTodo"
     @delete-todo="deleteTodo"
      />
@@ -15,7 +22,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
 
@@ -48,13 +55,25 @@ export default {
     const deleteTodo = (index) => {
       todos.value.splice(index, 1);
     };
-
+    
+    const searchText = ref('');
+    const filteredTodos = computed(() => {
+      if(searchText.value){
+        return todos.value.filter(todo =>{
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      
+      return todos.value;
+    });
     return {
       todos,
       addTodo,
       todoStyle,
       deleteTodo,
       toggleTodo,
+      searchText,
+      filteredTodos,
     };
   },
 };
