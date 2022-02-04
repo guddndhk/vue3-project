818b1e7cc1692a1fd9647dc7a9f25d53c7cb784f
guddndhk <guddndhk@naver.com>
2022-02-05 02:20:29 +0900

    22/02/05 axios post() 를 이용하여 jason server 연동완료.

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
      >
      <hr />
     <todo-simple-form @add-todo="addTodo" />
+    <div style="color: red">{{ error }}</div>
 
     <div v-if="!filteredTodos.length">
       There is nothing to display
@@ -25,6 +26,7 @@
 import { ref, computed } from "vue";
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue';
+import axios from 'axios';
 
 export default {
   components: {
@@ -32,18 +34,29 @@ export default {
     TodoList,
   },
   setup() {
-    
     const todos = ref([]);
-    
+    const error = ref('');
+
     const todoStyle = {
       textDecoration: "line-through",
       color: "gray",
     };
 
     const addTodo = (todo) => {
-      console.log(todo)
-      todos.value.push(todo);
-    
+      // 데이터베이스에 투두를 저장
+      error.value = '';
+      axios.post('http://localhost:3000/todos', {
+        subject: todo.subject,
+        completed: todo.completed,
+      //요청이 끝나고 오기전에 그다음으로 넘어가기에 프로미스가 리턴이 왔을때 then 키워드를 사용하여 요청이 끝났을때 응답이 왔을때 실행이 되게함
+      }).then(res => {
+        // 이부분을 밖에 두게되면 axios.post 작업이 끝났는지 아닌지 알수도 없을떄 추가가 되기때문에.. (비동기) ajax 와 비슷하다.
+        console.log(res)
+        todos.value.push(res.data);
+      }).catch(err => {
+        console.log(err)
+        error.value = '컴퓨터는 멍청하다 물 좀 줘 라고했니? 물을 어떻게 어디서 누구에게 어떻게 가져다줘 라고해야지..찾아봐'
+      });
     };
 
     const toggleTodo = (index) => {
@@ -74,6 +87,7 @@ export default {
       toggleTodo,
       searchText,
       filteredTodos,
+      error,
     };
   },
 };
