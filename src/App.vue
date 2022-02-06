447a0b6e8d3e2deb3398f69509370058bf6b88dd
guddndhk <guddndhk@naver.com>
2022-02-07 01:27:35 +0900

    22/02/07 Pagination 구현 완료.

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,27 @@
     @toggle-todo="toggleTodo"
     @delete-todo="deleteTodo"
      />
+    <hr />
+    <nav aria-label="Page navigation example">
+      <ul class="pagination">
+        <li v-if="currentPage !== 1" class="page-item">
+          <a style="cursor : pointer" class="page-link" @click="getTodos(currentPage - 1)">
+            Previous
+            </a>
+        </li>
+        <li 
+          v-for="page in numberOfPages"
+          :key="page"
+          class="page-item"
+          :class="currentPage == page ? 'active' : ''"
+        >
+          <a style="cursor : pointer"  class="page-link" @click="getTodos(page)">{{page}}</a>
+        </li>
+        <li v-if="numberOfPages !== currentPage" class="page-item">
+          <a style="cursor : pointer"  class="page-link" @click="getTodos(currentPage + 1)">Next</a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
@@ -36,10 +57,21 @@ export default {
   setup() {
     const todos = ref([]);
     const error = ref('');
+    const numberOfTodos = ref(0);
+    const limit = 5;
+    const currentPage = ref(1);
 
-    const getTodos = async () => {
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
+
+    const getTodos = async (page = currentPage.value) => {
+      currentPage.value = page;
       try{
-        const res = await axios.get('http://localhost:3000/todos')
+        const res = await axios.get(
+          `http://localhost:3000/todos?_page=${page}&_limit=${limit}`
+        );
+        numberOfTodos.value = res.headers['x-total-count'];
         todos.value = res.data;
       } catch (err) {
         console.log(err);
@@ -126,7 +158,9 @@ export default {
       searchText,
       filteredTodos,
       error,
-      
+      numberOfPages,
+      currentPage,
+      getTodos,
     };
   },
 };
