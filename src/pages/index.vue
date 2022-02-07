f8cbcaf04a13af35022909db7c880396512c3219
guddndhk <guddndhk@naver.com>
2022-02-08 03:51:02 +0900

    22/02/08 vue router 기본 셋팅 완료.

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Home page</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from '../pages/index.vue';
+import Todos from '../pages/todos/index.vue';
+
+//옵션
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path:'/',
+            name:'Home',
+            component: Home
+        },
+        {
+            path:'/todos',
+            name:'Todos',
+            component: Todos
+        }
+    ]
+});
+
+// 1 / home 2 /todos 3 /todos/create 4 /todos/:id 
+
+export default router;
