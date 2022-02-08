8c04f91113e557ffa522b4bf4a05e61d48de1453
guddndhk <guddndhk@naver.com>
2022-02-09 03:29:33 +0900

    22/02/09 Navbar 추가 작업.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Jay Cake</a>
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Todos</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   <router-view/>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
 <style>
