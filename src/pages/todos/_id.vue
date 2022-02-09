3d29a11493199f9d3c770957d2c979314d9c8cc4
guddndhk <guddndhk@naver.com>
2022-02-10 04:00:05 +0900

    22/02/09 To-do Page 에서 Status,cancel 기능 및 버튼 추가완료.

--- src/pages/todos/_id.vue
+++ src/pages/todos/_id.vue
@@ -4,22 +4,55 @@
     Loding...
   </div>
   <form v-else>
-    <div class="form-group">
-      <label>Todo Subject</label>
-      <input v-model="todo.subject" type="text" class="form-control">
+    <div class="row">
+      <div class="col-6">
+        <div class="form-group">
+          <label>Subject</label>
+          <input
+              v-model="todo.subject"
+              type="text"
+              class="form-control"
+          >
+        </div>
+      </div>
+      <div class="col-6">
+        <div class="form-group">
+          <label>Status</label>
+          <div>
+            <button
+                class="btn"
+                type="button"
+                :class="todo.completed ? 'btn-success' : 'btn-danger'"
+                @click="toggleTodoStatus"
+            >
+              {{ Incomplete ? 'Completed' : 'Incompleted' }}
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    <button class="btn btn-primary">Save</button>
+
+    <button type="submit" class="btn btn-primary">
+      Save
+    </button>
+    <button
+        class="btn btn-outline-dark ml-2"
+        @click="moveToTodoListPage"
+    >
+      Cancel
+    </button>
   </form>
 </template>
 
 <script>
-import {useRoute} from 'vue-router';
+import {useRoute, useRouter} from 'vue-router';
 import axios from "axios";
 import {ref} from "vue";
 
 export default {
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const todo = ref(null);
     const loding = ref(true);
 
@@ -31,16 +64,29 @@ export default {
       loding.value = false;
     };
 
+    const toggleTodoStatus = () => {
+      todo.value.completed = !todo.value.completed;
+    };
+
+    const moveToTodoListPage = () => {
+      router.push({
+        name: 'Todos'
+      })
+    };
+
     getTodo();
 
     return {
       todo,
       loding,
+      toggleTodoStatus,
+      moveToTodoListPage,
     }
   }
 }
 
 </script>
+
 
 <style scoped>
 
