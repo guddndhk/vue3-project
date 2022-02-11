5a8b34a1109314bba20bc4d4a6151e0320ebebba
guddndhk <guddndhk@naver.com>
2022-02-12 04:11:58 +0900

    22/02/12 Toast 기능 수정 transition 을 사용하여 애니메이션 효과 추가.

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -60,11 +60,13 @@
       Cancel
     </button>
   </form>
-  <Toast
-      v-if="showToast"
-      :message="toastMessage"
-      :type="toastAlertType"
-  />
+  <transition name="fade">
+    <Toast
+        v-if="showToast"
+        :message="toastMessage"
+        :type="toastAlertType"
+    />
+  </transition>
 </template>
 
 <script>
@@ -177,7 +179,7 @@ export default {
         }
         if (props.editing) {
           res = await axios.put(`http://localhost:3000/todos/${todoId}
-          `,data);
+          `, data);
           originalTodo.value = {...res.data};
         } else {
           res = await axios.post(`
@@ -213,5 +215,22 @@ export default {
 
 
 <style scoped>
+/* Toast 애니메이션
+*/
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
 
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
+
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+  transform: translateY(0px);
+}
 </style>
