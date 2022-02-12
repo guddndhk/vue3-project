d5c055732b30fdbe8db464da0b5d00522686abf7
guddndhk <guddndhk@naver.com>
2022-02-13 02:58:12 +0900

    22/02/12 toRefs 실험.

---
+++ src/composables/count.js
@@ -0,0 +1,8 @@
+import {reactive, toRefs} from "vue";
+
+export const useCount = () => {
+    const state = reactive({
+        count: 0
+    });
+    return toRefs(state);
+}
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>Home page</div>
+  <div>{{ count }}</div>
+  <button @click="count++">add</button>
 </template>
 
 <script>
-export default {
+import {useCount} from "@/composables/count";
 
+export default {
+  setup() {
+    const {count} = useCount();
+
+    return {
+      count
+    }
+  }
 }
 </script>
 
