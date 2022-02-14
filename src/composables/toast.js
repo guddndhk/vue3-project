d35d3d2c1ab1be93e901e71105210ff76b3392f6
guddndhk <guddndhk@naver.com>
2022-02-14 22:51:20 +0900

    22/02/14 Vuex 을 사용하여 모든 페이지에서 toast 기능을 확인가능 또한 컴포넌트로 관리 하기에 코드의 접근 용이성 및 유지보수 용이.

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -1,29 +1,32 @@
-import {ref , onUnmounted} from 'vue';
+import {computed} from 'vue';
+import {useStore} from "vuex";
 
 export const useToast = () => {
     //toast
-    const toastMessage = ref('');
-    const toastAlertType = ref('');
-    const showToast = ref(false);
-    const timeOut = ref(null);
+    const store = useStore();
+    const toastMessage = computed(() => store.getters.toastMessageWithSmile);
+    const toastAlertType = computed(() => store.state.toastAlertType);
+    const showToast = computed(() => store.state.showToast);
+    // const timeOut = computed(() => store.state.timeOut);
 
     const triggerToast = (message, type = 'success') => {
-        toastMessage.value = message;
-        toastAlertType.value = type;
-        showToast.value = true;
-        timeOut.value = setTimeout(() => {
-            toastMessage.value = '';
-            toastAlertType.value = '';
-            showToast.value = false;
-        }, 3000);
+        // toastMessage.value = message;
+        // toastAlertType.value = type;
+        // showToast.value = true;
+        // timeOut.value = setTimeout(() => {
+        //     toastMessage.value = '';
+        //     toastAlertType.value = '';
+        //     showToast.value = false;
+        // }, 3000);
+        store.dispatch('triggerToast', message, type);
     }
 
-    onUnmounted(() => {
-        console.log('unmounted')
-        clearTimeout(timeOut.value);
-    });
+    // onUnmounted(() => {
+    //     console.log('unmounted')
+    //     clearTimeout(timeOut.value);
+    // });
 
-    return{
+    return {
         toastMessage,
         toastAlertType,
         showToast,
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,47 @@
 import {createStore} from "vuex";
 
 export default createStore({
-    state:{
+    state: {
+        toastMessage: '',
+        toastAlertType: '',
+        showToast: false,
+        timeOut: null
+    },
+    mutations: {
+        UPDATE_TOAST_MESSAGE(state, payload) {
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE(state, payload) {
+            state.toastAlertType = payload;
+        },
+        UPDATE_TOAST_STATUS(state, payload) {
+            state.showToast = payload;
+        }
+    },
+    actions: {
+        triggerToast(context, message, type = 'success') {
+            // toastMessage.value = message;
+            context.commit('UPDATE_TOAST_MESSAGE', message)
+            // toastAlertType.value = type;
+            context.commit('UPDATE_TOAST_ALERT_TYPE', type)
+            // showToast.value = true;
+            context.commit('UPDATE_TOAST_STATUS', true)
 
+            setTimeout(() => {
+                // toastMessage.value = '';
+                context.commit('UPDATE_TOAST_MESSAGE', '')
+                // toastAlertType.value = '';
+                context.commit('UPDATE_TOAST_ALERT_TYPE', '')
+                // showToast.value = false;
+                context.commit('UPDATE_TOAST_STATUS', false)
+            }, 3000);
+
+        }
+    },
+    //computed 와 같은기능
+    getters: {
+        toastMessageWithSmile (state) {
+            return state.toastMessage + '^_^';
+        }
     }
 });
